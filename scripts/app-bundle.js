d685b887bcdac159a05df1d319090fbff7011f4b
Esger thuis <esgerj@gmail.com>
2017-09-27 23:08:19 +0200

    For starters

---
+++ scripts/app-bundle.js
@@ -0,0 +1,113 @@
+define('app',['exports', 'aurelia-framework', 'jquery'], function (exports, _aureliaFramework, _jquery) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    exports.App = undefined;
+
+    var _jquery2 = _interopRequireDefault(_jquery);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var App = exports.App = function () {
+        function App() {
+            _classCallCheck(this, App);
+
+            this.message = 'Snake by ashWare';
+            this.$arena = (0, _jquery2.default)('.arena');
+            this.snake = {
+                segments: []
+            };
+            this.initSnake();
+        }
+
+        App.prototype.initSnake = function initSnake() {
+            var self = this;
+            var canvasCenter = {
+                x: self.$arena.width(),
+                y: self.$arena.height()
+            };
+            var head = {
+                img: '/images/head.png',
+                x: canvasCenter.x,
+                y: canvasCenter.y
+            };
+        };
+
+        return App;
+    }();
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  Promise.config({
+    longStackTraces: _environment2.default.debug,
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
+
+  function configure(aurelia) {
+    aurelia.use.standardConfiguration().feature('resources');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n    <require from=\"reset.css\"></require>\n    <require from=\"app.css\"></require>\n    <h1 class=\"gameTitle\">${message}</h1>\n    <canvas class=\"arena\"></canvas>\n</template>"; });
+define('text!app.css', ['module'], function(module) { module.exports = "body {\n    position        : relative;\n    background-color: black;\n}\n\n.gameTitle {\n    position      : absolute;\n    top           : 0;\n    width         : 100vw;\n    font-family   : 'Trebuchet MS', sans-serif;\n    letter-spacing: 1px;\n    font-size     : 20px;\n    line-height   : 30px;\n    text-align    : center;\n    color         : whitesmoke;\n}\n\n.arena {\n    width     : 100vw;\n    height    : 100vh;\n    box-sizing: border-box;\n    border    : 30px solid crimson;\n}\n"; });
+define('text!reset.css', ['module'], function(module) { module.exports = "/* http://meyerweb.com/eric/tools/css/reset/ \n   v2.0 | 20110126\n   License: none (public domain)\n*/\n\na, abbr, acronym, address, applet, article, aside, audio, b, big, blockquote, body, canvas, caption, center, cite, code, dd, del, details, dfn, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, header, hgroup, html, i, iframe, img, ins, kbd, label, legend, li, mark, menu, nav, object, ol, output, p, pre, q, ruby, s, samp, section, small, span, strike, strong, sub, summary, sup, table, tbody, td, tfoot, th, thead, time, tr, tt, u, ul, var, video {\n    margin        : 0;\n    padding       : 0;\n    border        : 0;\n    font-size     : 100%;\n    font          : inherit;\n    vertical-align: baseline;\n}\n/* HTML5 display-role reset for older browsers */\narticle, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {\n    display: block;\n}\n\nbody {\n    line-height: 1;\n}\n\nol, ul {\n    list-style: none;\n}\n\nblockquote, q {\n    quotes: none;\n}\n\nblockquote:after, blockquote:before, q:after, q:before {\n    content: '';\n    content: none;\n}\n\ntable {\n    border-collapse: collapse;\n    border-spacing : 0;\n}\n"; });
+//# sourceMappingURL=app-bundle.js.map
--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
+import {
+    inject,
+    bindable
+} from 'aurelia-framework';
+
+import $ from 'jquery';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+
+    constructor() {
+        this.message = 'Snake by ashWare';
+        this.$arena = $('.arena');
+        this.snake = {
+            segments: []
+        }
+        this.initSnake();
+    }
+
+    initSnake() {
+        let self = this;
+        let canvasCenter = {
+            x: self.$arena.width(),
+            y: self.$arena.height()
+        };
+        let head = {
+            img: '/images/head.png',
+            x: canvasCenter.x,
+            y: canvasCenter.y
+        }
+    }
 }
